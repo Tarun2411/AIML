3ca3cf28b0d4084b05453d786db4e1e8f95738bd
Tarun2411 <51094722+Tarun2411@users.noreply.github.com>
2020-06-08 12:36:53 +0530

    Add files via upload

---
+++ assignment/fraction.py
@@ -0,0 +1,10 @@
+def fracdis(a,b):
+    return a/b
+def add(a,b,c,d):
+    return (a/b)+(c/d)
+def sub(a,b,c,d):
+    return (a/b)-(c/d)
+def mul(a,b,c,d):
+    return (a/b)*(c/d)
+def div(a,b,c,d):
+    return (a/b)/(c/d)
